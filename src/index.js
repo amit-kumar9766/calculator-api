3ae759559d09e31f02d78e262287f394adeb2ce6
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-09-14 13:04:59 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -14,8 +14,79 @@ app.use(bodyParser.json())
 // your code goes here
 
 // here
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.post('/add',(req,res)=>{
+   
+    const x=req.body.x;
+    const y=req.body.y;
+
+    if(typeof(x) ==="string" || typeof(y) === "string" ){
+        res.json({status:'failure',message: "Invalid data types"})
+    }
+   
+    else if(x>1000000 || y>1000000||x+y>1000000){
+        res.json({status:'error',message: "Overflow"})
+    }
+  
+    //const z=x+y;
+    res.json({status:'success',message: "the sum of given two numbers",sum:x+y})
+   
+})
+
+app.post('/sub',(req,res)=>{
+    const x=req.body.x;
+    const y=req.body.y;
+
+    if(typeof(x) ==="string" || typeof(y) === "string" ){
+        res.json({status:'failure',message: "Invalid data types"})
+    }
+   
+    else if(x-y<-1000000){
+        res.json({status:'error',message: "Underflow"})
+    }
+    // else if(y<1000000){
+    //     res.json({status:'error',message: "Underflow"})
+    // }
+    //const z=x+y;
+    res.json({status:'success',message: "the difference of given two numbers",difference:x-y})
+})
+
+app.post('/multiply',(req,res)=>{
+    const x=req.body.x;
+    const y=req.body.y;
+
+    if(typeof(x) ==="string" || typeof(y) === "string" ){
+        res.json({status:'failure',message: "Invalid data types"})
+    }
+   
+    else if(x*y>1000000 ){
+        res.json({status:'error',message: "Overflow"})
+    }
+    // else if(y>1000000){
+    //     res.json({status:'error',message: "Overflow"})
+    // }
+    //const z=x+y;
+    res.json({status:'success',message: "The product of given two numbers",result:x*y})
+    
+
+})
+
+app.post('/divide',(req,res)=>{
+    const x=req.body.x;
+    const y=req.body.y;
+    if(typeof(x) ==="string" || typeof(y) === "string" ){
+        res.json({status:'failure',message: "invalid data types"})
+    }
+    else if (y==0){
+        res.send('Cannot divide by 0')
+    }
+    res.send({status:'success',message: "The division of given two numbers",result:x/y})
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-module.exports = app;
+module.exports = app; 
