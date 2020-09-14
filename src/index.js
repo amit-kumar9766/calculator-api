33afba42fa61cbf6298727d44c216c336f5b7215
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-09-14 13:14:37 +0530

    updated1

--- src/index.js
+++ src/index.js
@@ -24,15 +24,15 @@ app.post('/add',(req,res)=>{
     const y=req.body.y;
 
     if(typeof(x) ==="string" || typeof(y) === "string" ){
-        res.json({status:'failure',message: "Invalid data types"})
+       return  res.json({status:'failure',message: "Invalid data types"})
     }
    
     else if(x>1000000 || y>1000000||x+y>1000000){
-        res.json({status:'error',message: "Overflow"})
+        return res.json({status:'error',message: "Overflow"})
     }
   
     //const z=x+y;
-    res.json({status:'success',message: "the sum of given two numbers",sum:x+y})
+   return  res.json({status:'success',message: "the sum of given two numbers",sum:x+y})
    
 })
 
@@ -41,17 +41,17 @@ app.post('/sub',(req,res)=>{
     const y=req.body.y;
 
     if(typeof(x) ==="string" || typeof(y) === "string" ){
-        res.json({status:'failure',message: "Invalid data types"})
+       return res.json({status:'failure',message: "Invalid data types"})
     }
    
     else if(x-y<-1000000){
-        res.json({status:'error',message: "Underflow"})
+       return res.json({status:'error',message: "Underflow"})
     }
     // else if(y<1000000){
     //     res.json({status:'error',message: "Underflow"})
     // }
     //const z=x+y;
-    res.json({status:'success',message: "the difference of given two numbers",difference:x-y})
+    return res.json({status:'success',message: "the difference of given two numbers",difference:x-y})
 })
 
 app.post('/multiply',(req,res)=>{
@@ -59,17 +59,17 @@ app.post('/multiply',(req,res)=>{
     const y=req.body.y;
 
     if(typeof(x) ==="string" || typeof(y) === "string" ){
-        res.json({status:'failure',message: "Invalid data types"})
+        return res.json({status:'failure',message: "Invalid data types"})
     }
    
     else if(x*y>1000000 ){
-        res.json({status:'error',message: "Overflow"})
+       return  res.json({status:'error',message: "Overflow"})
     }
     // else if(y>1000000){
     //     res.json({status:'error',message: "Overflow"})
     // }
     //const z=x+y;
-    res.json({status:'success',message: "The product of given two numbers",result:x*y})
+    return res.json({status:'success',message: "The product of given two numbers",result:x*y})
     
 
 })
@@ -78,12 +78,12 @@ app.post('/divide',(req,res)=>{
     const x=req.body.x;
     const y=req.body.y;
     if(typeof(x) ==="string" || typeof(y) === "string" ){
-        res.json({status:'failure',message: "invalid data types"})
+        return res.json({status:'failure',message: "invalid data types"})
     }
     else if (y==0){
-        res.send('Cannot divide by 0')
+       return  res.send('Cannot divide by 0')
     }
-    res.send({status:'success',message: "The division of given two numbers",result:x/y})
+    return res.send({status:'success',message: "The division of given two numbers",result:x/y})
 })
 
 
